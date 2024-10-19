5ef2fd618d70ec0dd19c632d4e42ce452da3be6f
SamHalvoe <46531812+SamHalvoe@users.noreply.github.com>
2024-10-19 14:53:17 +0200

    WIP: SerialAudioInterface

--- SerialInterface.hpp
+++ SerialInterface.hpp
@@ -34,7 +34,24 @@ namespace halvoe
            size_t tc_deserializerBufferSize>
   class SerialInterface
   {
+    public:
+      static const uint16_t c_interfaceTag = 0x5A47;
+      static const uint16_t c_interfaceVersion = 0x0001;
+
+      struct OutMessage
+      {
+        public:
+          Serializer<tc_serializerBufferSize> m_serializer;
+          SerializerReference<SerialMessageSizeType> m_messageSize;
+
+        public:
+          OutMessage(Serializer<tc_serializerBufferSize>&& in_serializer, SerializerReference<SerialMessageSizeType>&& in_messageSize) :
+            m_serializer(std::move(in_serializer)), m_messageSize(std::move(in_messageSize))
+          {}
+      };
+
     private:
+      HardwareSerial& m_serial;
       std::array<uint8_t, tc_serializerBufferSize> m_serializerBuffer;
       std::array<uint8_t, tc_deserializerBufferSize> m_deserializerBuffer;
 
@@ -55,26 +72,37 @@ namespace halvoe
       }
       
     public:
-      SerialInterface() = default;
+      SerialInterface(HardwareSerial& in_serial) : m_serial(in_serial)
+      {}
 
       // Implementer has to config and "begin" Serial port.
       virtual bool setup() = 0;
 
-      void send()
+      OutMessage beginMessage(SerialMessageType in_messageType)
       {
         Serializer<tc_serializerBufferSize> serialzer(m_serializerBuffer);
-
+        auto messageSize = serialzer.template skip<SerialMessageSizeType>();
+        serialzer.template write<uint16_t>(c_interfaceTag);
+        serialzer.template write<uint16_t>(c_interfaceVersion);
+        serialzer.template writeEnum<SerialMessageType>(in_messageType);
+        return OutMessage(std::move(serialzer), std::move(messageSize));
+      }
+      
+      size_t sendMessage(OutMessage&& in_message)
+      {
+        in_message.m_messageSize.write(in_message.m_serializer.template getBytesWritten() - sizeof(SerialMessageSizeType));
+        return m_serial.write(m_serializerBuffer.data(), in_message.m_serializer.template getBytesWritten());
       }
 
-      bool receive()
+      bool receiveMessage()
       {
-        if (Serial1.available() < sizeof(SerialMessageSizeType)) { return false; }
+        if (m_serial.available() < sizeof(SerialMessageSizeType)) { return false; }
         
         Deserializer<tc_deserializerBufferSize> deserializer(m_deserializerBuffer);
-        Serial1.readBytes(m_deserializerBuffer.data(), sizeof(SerialMessageSizeType));
+        m_serial.readBytes(m_deserializerBuffer.data(), sizeof(SerialMessageSizeType));
         SerialMessageSizeType messageSize = deserializer.template read<SerialMessageSizeType>();
-        size_t bytesReceived = Serial1.readBytes(m_deserializerBuffer.data() + deserializer.getBytesRead(),
-                                                 messageSize - deserializer.getBytesRead());
+        size_t bytesReceived = m_serial.readBytes(m_deserializerBuffer.data() + deserializer.getBytesRead(),
+                                                  messageSize - deserializer.getBytesRead());
         
         if (bytesReceived != messageSize) { return false; }
 
@@ -82,7 +110,7 @@ namespace halvoe
         // ToDo: Check tag!
         auto interfaceVersion = deserializer.template read<uint16_t>();
         // ToDo: Check version!
-        auto serialMessageType = SerialMessageType{ deserializer.template read<std::underlying_type<SerialMessageType>::type>() };
+        auto serialMessageType = deserializer.template readEnum<SerialMessageType>();
 
         switch (serialMessageType)
         {
--- halvoeAudioBoardSerialInterface.ino
+++ halvoeAudioBoardSerialInterface.ino
@@ -6,14 +6,60 @@
     Author:     LAPTOP-8TKMEDGB\samue
 */
 
+#include "SerialInterface.hpp"
+
+using namespace halvoe;
+
+static constexpr const size_t c_serializerBufferSize = 1024;
+static constexpr const size_t c_deserializerBufferSize = 1024;
+
+class SerialImpl : public SerialInterface<c_serializerBufferSize, c_deserializerBufferSize>
+{
+public:
+	SerialImpl(HardwareSerial& in_serial) : SerialInterface<c_serializerBufferSize, c_deserializerBufferSize>(in_serial)
+	{}
+	
+	bool setup()
+	{
+		return true;
+	}
+
+private:
+	bool doHandleData(Deserializer<c_deserializerBufferSize>&& in_deserializer, SerialDataCode in_code)
+	{
+		return false;
+	}
+
+	bool doHandleCommand(Deserializer<c_deserializerBufferSize>&& in_deserializer, SerialCommandCode in_code)
+	{
+		switch (in_code)
+		{
+			case SerialCommandCode::playFile:
+				SerialStringSizeType filenameSize = 0;
+				auto filenamePointer = in_deserializer.read<SerialStringSizeType>(64, filenameSize);
+				String filename(filenamePointer.get(), filenameSize);
+				return true;
+		}
+
+		return false;
+	}
+};
+
+SerialImpl serialImpl(Serial1);
+String string;
+
 // The setup() function runs once each time the micro-controller starts
 void setup()
 {
-
+	serialImpl.setup();
+	string = "Hello World!";
 }
 
 // Add the main program code into the continuous loop() function
 void loop()
 {
-
+	serialImpl.receiveMessage();
+	auto message = serialImpl.beginMessage(SerialMessageType::command);
+	message.m_serializer.write(string.c_str(), string.length());
+	serialImpl.sendMessage(std::move(message));
 }
